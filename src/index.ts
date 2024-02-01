5a1939bdcbeef42af84c3574d6a2a9b5f491932d
Jean Pierre <pgdick@gmail.com>
2024-01-31 21:11:00 -0300

    fix: remove redis and sentury

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,6 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { AppModule } from './app.module';
 import { CacheRepository } from './database';
 import { ErrorHandlerMiddleware } from 'middlewares/errors/error-handler.middleware';
-import * as Sentry from '@sentry/node';
-import { dataSource } from '@database/postgres/datasource';
 
 async function initializeApp() {
   const app = await NestFactory.create<NestFastifyApplication>(
@@ -30,27 +28,8 @@ async function initializeApp() {
   return app;
 }
 
-
-async function initializeCache() {
-  CacheRepository.connect({
-    host: process.env.REDIS_HOST as string,
-    port: process.env.REDIS_PORT as string,
-  })
-}
-
-function initSentry() {
-  Sentry.init({
-    dsn: process.env.SENTRY_DSN,
-    environment: process.env.SENTRY_ENV,
-    integrations: [new Sentry.Integrations.Http({ tracing: true })],
-    tracesSampleRate: 0.0
-  })
-}
-
 async function bootstrap() {
   const app = await initializeApp();
-  await initializeCache();
-  initSentry()
 
   await app.listen(Number(process.env.PORT));
   console.log(`@db-software/vote-api started on port ${process.env.PORT}`)
